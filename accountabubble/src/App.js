dca86955d4373f7ecdcf47bb17f8a858358d3346
Megan <calvem2@cs.washington.edu>
2021-01-15 21:06:20 -0800

    progress bar.

--- accountabubble/src/App.js
+++ accountabubble/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Bubble from "./Bubble";
+import ProgressBar from "./ProgressBar";
 
 function App() {
+  let tasks = [{name:"yoga", assignee:"ash", completed:false}, {name:"legos", assignee:"claire", completed:true}];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProgressBar tasks={tasks}/>
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.js</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
     </div>
   );
 }
---
+++ accountabubble/src/ProgressBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class ProgressBar extends Component {
+
+    render() {
+        let completed = this.props.tasks.filter(task => {
+            return task.completed;
+        });
+
+        return (
+            <div>
+                <progress value={completed.length} max={this.props.tasks.length}></progress>
+            </div>
+        );
+    }
+}
+
+export default ProgressBar;
